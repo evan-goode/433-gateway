03929bc948e8ffe6bee2e23d6ee5ef01db049ce4
Evan Goode <mail@evangoo.de>
2019-10-13 18:20:01 -0400

    change license in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url="https://github.com/evan-goode/433-gateway",
     author="Evan Goode",
     author_email="mail@evangoo.de",
-    license="GPLv3",
+    license="AGPLv3",
     install_requires=["rpi-rf", "paho-mqtt", "toml"],
     packages=find_packages(),
     entry_points={"console_scripts": ["433-gateway=433.433:main"]},
